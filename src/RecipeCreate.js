0a9e78297023a8594d42e3b37d4cea464844d75c
Matthew Lance <matthewlance55@gmail.com>
2025-04-16 17:27:55 -0400

    final submission

--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -1,17 +1,81 @@
 import React, { useState } from "react";
 
-function RecipeCreate() {
+function RecipeCreate({createNewRecipe}) {
+const initialRecipeData ={
+  name: "",
+  cuisine: "",
+  photo: "",
+  ingredients: "",
+  preparation: "",
+};
+
+const [recipeData, setRecipeData] = useState(initialRecipeData);
+
+const handleChange = ({target}) =>{
+  setRecipeData({
+    ...recipeData,
+    [target.name]: target.value,
+  });
+}
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  createNewRecipe(recipeData);
+  setRecipeData(initialRecipeData);
+}
 
   // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
   // TODO: Add the required input and textarea form elements.
   // TODO: Add the required submit and change handlers.
   
   return (
-    <form name="create">
+    <form name="create" onSubmit={handleSubmit}>
       <table>
         <tbody>
           <tr>
-            <td></td>
+            <td>
+              <input
+                name="name"
+                type="text"
+                placeholder="Name"
+                value={recipeData.name}
+                onChange={handleChange}
+              />
+            </td>
+            <td>
+              <input
+                name="cuisine"
+                type="text"
+                placeholder="Cuisine"
+                value={recipeData.cuisine}
+                onChange={handleChange}
+                />
+            </td>
+            <td>
+              <input
+                name="photo"
+                type="url"
+                placeholder="Photo URL"
+                value={recipeData.photo}
+                onChange={handleChange}
+              />
+            </td>
+            <td>
+              <textarea 
+                name="ingredients"
+                placeholder="Ingredients"
+                value={recipeData.ingredients}
+                onChange={handleChange}
+              />
+            </td>
+            <td>
+              <textarea
+                name="preparation"
+                placeholder="Preparation"
+                value={recipeData.preparation}
+                onChange={handleChange}
+              />
+            </td>
             <td>
               <button type="submit">Create</button>
             </td>
--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,6 +1,26 @@
+//import { prepareCopy } from "immer/dist/internal.js";
 import React from "react";
 
-function RecipeList() {
+
+function RecipeView({recipe, deleteRecipe}){
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>{recipe.name}</td>
+          <td>{recipe.cuisine}</td>
+          <td><img src={recipe.photo} alt={recipe.name}/></td>
+          <td className="content_td"><p>{recipe.ingredients}</p></td>
+          <td className="content_td"><p>{recipe.preparation}</p></td>
+          <td><button name="delete" onClick={deleteRecipe}>Delete</button></td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
+
+function RecipeList({recipes, deleteRecipe}) {
   
   // TODO: Display the list of recipes using the structure of table that is provided.
   // TODO: Create at least one additional component that is used by this component.
@@ -11,12 +31,23 @@ function RecipeList() {
       <table>
         <thead>
           <tr>
-            <th></th>
+            <th>Name</th>
+            <th>Cuisine</th>
+            <th>Photo</th>
+            <th>Ingredients</th>
+            <th>Preparation</th>
             <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-        
+          {recipes.map((recipe, index) =>
+            <RecipeView
+              key={index}
+              index={index}
+              recipe={recipe}
+              deleteRecipe={()=>deleteRecipe(index)}
+            />
+          )}
         </tbody>
       </table>
     </div>
